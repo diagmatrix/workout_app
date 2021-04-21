036a064e04c465ab3272ced4bd50da8aae8515a3
diagmatrix <manuelgbal1@gmail.com>
2021-04-21 13:37:37 +0200

    Created url tree & a template for void plans

--- models/date_management.js
+++ models/date_management.js
@@ -5,17 +5,17 @@ const sub = require('date-fns/sub')
 // Calculates current week's Monday
 function this_monday() {
     var monday = startOfWeek(new Date(),{weekStartsOn: 1});
-    return monday.getFullYear() + "/" + (monday.getMonth()+1) + "/" + monday.getDate();
+    return monday.getFullYear() + "-" + (monday.getMonth()+1) + "-" + monday.getDate();
 }
 // Gets the following week
 function change_week(current,action) {
     switch(action) {
         case "next":
             var new_monday = add(new Date(current),{weeks: 1});
-            return new_monday.getFullYear() + "/" + (new_monday.getMonth()+1) + "/" + new_monday.getDate();
+            return new_monday.getFullYear() + "-" + (new_monday.getMonth()+1) + "-" + new_monday.getDate();
         case "prev":
             var new_monday = sub(new Date(current),{weeks: 1});
-            return new_monday.getFullYear() + "/" + (new_monday.getMonth()+1) + "/" + new_monday.getDate();
+            return new_monday.getFullYear() + "-" + (new_monday.getMonth()+1) + "-" + new_monday.getDate();
         default:
             return current; 
     }
